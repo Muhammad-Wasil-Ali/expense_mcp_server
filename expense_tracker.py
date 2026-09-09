80362969def82c5423fbc2bb571c0920bb04d848
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-09-09 16:47:45 +0500

    Update async issue

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,27 +1,38 @@
+import asyncio
 from fastmcp import FastMCP
-from supabase import create_client
+from supabase import create_async_client, AsyncClient
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-mcp=FastMCP("Expense Tracker")
+mcp = FastMCP("Expense Tracker")
 
-supabase=create_client(os.getenv("SUPABASE_URL"),os.getenv("SUPABASE_SERVICE_KEY"))
-
-
+supabase: AsyncClient | None = None
 
 
-# add expense 
+async def init_supabase():
+    global supabase
+    supabase = await create_async_client(
+        os.getenv("SUPABASE_URL"),
+        os.getenv("SUPABASE_SERVICE_KEY")
+    )
+
+
+# add expense
 @mcp.tool()
-async def add_expense(amount:float,category:str,description:str="")->dict:
+async def add_expense(amount: float, category: str, description: str = "") -> dict:
     """Add new expense record"""
-    
-    response=await supabase.table("expenses").insert({"amount":amount,"category":category,"description":description}).execute()
-    
+
+    response = await supabase.table("expenses").insert({
+        "amount": amount,
+        "category": category,
+        "description": description
+    }).execute()
+
     return response.data[0]
 
 
-
-if __name__=="__main__":
-    mcp.run(transport="http",host="0.0.0.0",port=8081)
+if __name__ == "__main__":
+    asyncio.run(init_supabase())
+    mcp.run(transport="http", host="0.0.0.0", port=8081)
