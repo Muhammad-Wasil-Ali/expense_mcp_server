9f4796d9da332ba8a41f05d6f18fcd4285b0b544
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-09-10 12:35:06 +0500

    trying fixed

--- api/index.py
+++ api/index.py
@@ -1,35 +1,41 @@
-import asyncio
 from fastmcp import FastMCP
 from supabase import create_async_client, AsyncClient
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 mcp = FastMCP("Expense Tracker")
 
 supabase: AsyncClient | None = None
 
 
-async def init_supabase():
+async def get_supabase() -> AsyncClient:
     global supabase
-    supabase = await create_async_client(
-        os.getenv("SUPABASE_URL"),
-        os.getenv("SUPABASE_SERVICE_KEY")
-    )
+
+    if supabase is None:
+        supabase = await create_async_client(
+            os.environ["SUPABASE_URL"],
+            os.environ["SUPABASE_SERVICE_KEY"],
+        )
+
+    return supabase
 
 
-# add expense
 @mcp.tool()
-async def add_expense(amount: float, category: str, description: str = "") -> dict:
+async def add_expense(
+    amount: float,
+    category: str,
+    description: str = ""
+) -> dict:
     """Add new expense record"""
 
-    response = await supabase.table("expenses").insert({
+    client = await get_supabase()
+
+    response = await client.table("expenses").insert({
         "amount": amount,
         "category": category,
-        "description": description
+        "description": description,
     }).execute()
 
     return response.data[0]
+
 
 app = mcp.http_app(path="/mcp")
